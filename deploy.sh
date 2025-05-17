79cb662bf3c473774738a35bac0edbf1e456b4d2
keamysh <keamysh@gmail.com>
2025-05-17 04:06:10 -0600

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Deploying webapp containers..."
+docker stop acada-webapp1 acada-webapp2 acada-webapp3 acada-webapp4 acada-webapp5 || true
+docker rm acada-webapp1  acada-webapp2 acada-webapp3 acada-webapp4 acada-webapp5 -f
+docker network create acada-app
+docker run -d --name acada-webapp1 --hostname acada-webapp1 --network acada-app keamysh/acadaweb:latest
+docker run -d --name acada-webapp2 --hostname acada-webapp2 --network acada-app keamysh/acadaweb:latest
+docker run -d --name acada-webapp3 --hostname acada-webapp3 --network acada-app keamysh/acadaweb:latest
+docker run -d --name acada-webapp4 --hostname acada-webapp4 --network acada-app keamysh/acadaweb:latest
+docker run -d --name acada-webapp5 --hostname acada-webapp5 --network acada-app keamysh/acadaweb:latest
+docker ps | grep -i acada-webapp*
+echo "Deploying webapp containers done"
+
+echo "Deploying HAproxy container..."
+docker rm haproxy -f >/dev/null 2>&1 || true
+docker run -d --name haproxy --network acada-app -v /tmp/haproxy.cfg:usr/local/etc/haproxy/haproxy.cfg:ro -p 9090:80 haproxy:latest
+docker ps |grep -i haproxy*
+echo "Deploying HAproxy container done"
