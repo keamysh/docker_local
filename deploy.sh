fa83c705048308b185af20c92d53432411934ce8
keamysh <keamysh@gmail.com>
2025-05-20 13:23:42 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker rm acada-webapp1  acada-webapp2 acada-webapp3 acada-webapp4 acada-webapp5
 docker network create acada-app
 docker run -d --name acada-webapp1 --hostname acada-webapp1 --network acada-app keamysh/acadaweb:latest
 docker run -d --name acada-webapp2 --hostname acada-webapp2 --network acada-app keamysh/acadaweb:latest
-docker run -d --name acada-webapp3 --hostname acada-webapp3 --network acada-app keamysh/acadaweb:latest
+docker run -d --name acada-webapp3 --hostname acada-webapp3 --network acada-app keamysh/acadaweb:latest 
 docker run -d --name acada-webapp4 --hostname acada-webapp4 --network acada-app keamysh/acadaweb:latest
 docker run -d --name acada-webapp5 --hostname acada-webapp5 --network acada-app keamysh/acadaweb:latest
 docker ps | grep -i acada-webapp*
